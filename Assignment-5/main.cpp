94c412ae12a3c61df25dabea315039033b921443
sweoo <swezinoo410@gmail.com>
2024-03-26 13:18:49 +0800

    add internal documentation for main.cpp file

--- Assignment-5/main.cpp
+++ Assignment-5/main.cpp
@@ -1,3 +1,10 @@
+/** @file main.cpp
+ * @author Swe Zin Oo
+ * @date 26 Mar 2024
+ * @brief The main.cpp file is the entry point of the program. It includes reading data from a file for morse code, building a morse code
+ * tree using that data, encoding and decoding a message using morse code tree.
+*/
+
 #include <iostream>
 #include <map>
 #include "string.h"
@@ -8,12 +15,13 @@ using namespace std;
 
 int main(){
 
+    
     map<char,string> morse_code_map = read_data_from_file("letter.txt");
     
 
     Binary_Tree<string>* morse_code_tree = new Binary_Tree<string>();
     morse_code_tree->build_morse_code_tree(morse_code_map);
-   // cout << morse_code_tree->to_string() << endl;
+    
 
     string encoded_message = morse_code_tree->encode_message("Hello World",morse_code_map);
     cout << "Coded message: " << encoded_message << endl;
