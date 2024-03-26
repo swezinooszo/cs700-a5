16c128083ec24a18eedd8500fde3269e95c0bff9
sweoo <swezinoo410@gmail.com>
2024-03-26 13:11:07 +0800

    add utils.h file

---
+++ Assignment-5/utils.h
@@ -0,0 +1,14 @@
+/** @file utils.h
+ * @author Swe Zin Oo
+ * @date 26 Mar 2024
+ * @brief This is the utility class of the program, emcompassing all functions necessary for building a binary tree.
+*/
+#pragma once
+
+#include <map>
+using namespace std;
+
+/** Read the data (letter and morse code) from a file
+ * @param vector vector to be sorted in ascending order
+*/ 
+map<char,string> read_data_from_file(string filename);
